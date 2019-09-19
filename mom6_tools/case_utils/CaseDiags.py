f046aa079b4b9f7b3d48d4b9a39a96c0630209f1
alperaltuntas <alperaltuntas@gmail.com>
2019-09-19 14:26:12 -0600

    finalize diag_table parser function

--- mom6_tools/case_utils/CaseDiags.py
+++ mom6_tools/case_utils/CaseDiags.py
@@ -7,6 +7,9 @@ DiagFileEntry = namedtuple('DiagFileEntry',
                     ("output_freq", "output_freq_units", "file_format", "time_axis_units",
                      "time_axis_name", "new_file_freq", "new_file_freq_units", "start_time",
                      "file_duration", "file_duration_units"))
+DiagFieldEntry = namedtuple('DiagFieldEntry',
+                    ("module_name", "output_name", "file_name", "time_sampling", "reduction_method",
+                     "regional_section", "packing"))
 
 class CaseDiags(object,):
 
@@ -48,28 +51,33 @@ class CaseDiags(object,):
                         
                     if within_file_list:
                         file_name = line_split[0]
-                        file_entry = DiagFileEntry(
-                            output_freq=line_split[1],
-                            output_freq_units=line_split[2],
-                            file_format=line_split[3],
-                            time_axis_units=line_split[4],
-                            time_axis_name=line_split[5],
-                            new_file_freq=line_split[6] if len(line_split)>6 else None,
-                            new_file_freq_units=line_split[7] if len(line_split)>7 else None,
-                            start_time=line_split[8] if len(line_split)>8 else None,
-                            file_duration=line_split[9] if len(line_split)>9 else None,
-                            file_duration_units=line_split[10] if len(line_split)>10 else None
+                        self.diag_files[file_name] = DiagFileEntry(
+                            output_freq = line_split[1],
+                            output_freq_units = line_split[2],
+                            file_format = line_split[3],
+                            time_axis_units = line_split[4],
+                            time_axis_name = line_split[5],
+                            new_file_freq = line_split[6] if len(line_split)>6 else None,
+                            new_file_freq_units = line_split[7] if len(line_split)>7 else None,
+                            start_time = line_split[8] if len(line_split)>8 else None,
+                            file_duration = line_split[9] if len(line_split)>9 else None,
+                            file_duration_units = line_split[10] if len(line_split)>10 else None
                         )
-                        self.diag_files[file_name] = file_entry
                                                 
                     else: # within field list
-                        field_name = line.split()[1]
-                        field_line = list(line.split())
-                        print(field_name, field_line)
-                        field_line.remove(field_name)
-                        self.diag_fields[field_name] = [entry for entry in field_line]
+                        field_name = line_split[1]
+                        self.diag_fields[field_name] = DiagFieldEntry(
+                            module_name = line_split[0],
+                            output_name = line_split[2],
+                            file_name = line_split[3],
+                            time_sampling = line_split[4],
+                            reduction_method = line_split[5],
+                            regional_section = line_split[6],
+                            packing = line_split[7])
 
 config_yml_path = "/glade/work/altuntas/mom6.diags/g.c2b6.GJRA.TL319_t061.long_JRA_mct.001/diag_config.yml"
 
 case_diags = CaseDiags(config_yml_path)
 case_diags._parse_diag_table()
+for entry in case_diags.diag_files:
+    print(entry,case_diags.diag_files[entry])
