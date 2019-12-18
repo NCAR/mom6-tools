da0031b22acbfe0480f3e1cc9c1f75f174a95d02
Gustavo Marques <gmarques@ucar.edu>
2019-12-18 14:47:44 -0700

    Add option to call this module via command line

--- mom6_tools/poleward_heat_transport.py
+++ mom6_tools/poleward_heat_transport.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 
-import io
-import netCDF4
-import numpy
+import io, yaml, os
 import matplotlib.pyplot as plt
-import os
-import sys
-import warnings
+import warnings, dask, numpy, netCDF4
+from datetime import datetime, date
 import xarray as xr
-
+from mom6_tools.DiagsCase import DiagsCase
+from ncar_jobqueue import NCARCluster
+from dask.distributed import Client
 from mom6_tools import m6plot
 from mom6_tools  import m6toolbox
 from mom6_tools.MOM6grid import MOM6grid
@@ -16,16 +15,19 @@ from mom6_tools.MOM6grid import MOM6grid
 def options():
   try: import argparse
   except: raise Exception('This version of python is not new enough. python 2.7 or newer is required.')
-  parser = argparse.ArgumentParser(description='''Script for plotting plotting poleward heat transport.''')
-  parser.add_argument('infile', type=str, help='''Path to the file(s) to be processed (i.e <some_path>/run/)''')
-  parser.add_argument('-l','--label', type=str, default='', help='''Label to add to the plot.''')
-  parser.add_argument('-n','--case_name', type=str, default='', help='''Case name.  Default is to read from netCDF file.''')
-  parser.add_argument('-o','--outdir', type=str, default='.', help='''Directory in which to place plots.''')
-  parser.add_argument('-m','--monthly', type=str, default='.', help='''Monthly-averaged file containing 3D 'T_ady_2d' and 'T_diffy_2d''')
-  parser.add_argument('-s','--static', type=str, required=True, help='''Name of the MOM6 static file.''')
-  parser.add_argument('-savefigs', help='''Save figures in a PNG format.''', action="store_true")
-  parser.add_argument('-year_start', type=int, default=80, help='''Start year to compute averages. Default is 80.''')
-  parser.add_argument('-year_end', type=int, default=100, help='''End year to compute averages. Default is 100.''')
+  parser = argparse.ArgumentParser(description='''Script for plotting poleward heat transport.''')
+  parser.add_argument('diag_config_yml_path', type=str, help='''Full path to the yaml file  \
+    describing the run and diagnostics to be performed.''')
+  parser.add_argument('-v', '--variables', nargs='+', default=['T_ady_2d', 'T_diffy_2d'],
+                     help='''Variables to be processed (default=['T_ady_2d', 'T_diffy_2d'])''')
+  parser.add_argument('-sd','--start_date',  type=str, default='0001-01-01',
+                     help='''Start year to plot (default=0001-01-01)''')
+  parser.add_argument('-ed','--end_date',   type=str, default='0100-12-31',
+                      help='''Final year to plot (default=0100-12-31)''')
+  parser.add_argument('-nw','--number_of_workers',  type=int, default=2,
+                      help='''Number of workers to use (default=2).''')
+  parser.add_argument('-debug',   help='''Add priting statements for debugging purposes''',
+                      action="store_true")
 
   cmdLineArgs = parser.parse_args()
   return cmdLineArgs
@@ -33,27 +35,74 @@ def options():
 def main(stream=False):
   # Get options
   args = options()
-  # mom6 grid
-  grd = MOM6grid(args.infile+args.static)
+  nw = args.number_of_workers
+  if not os.path.isdir('PNG'):
+    print('Creating a directory to place figures (PNG)... \n')
+    os.system('mkdir PNG')
+  if not os.path.isdir('ncfiles'):
+    print('Creating a directory to place figures (ncfiles)... \n')
+    os.system('mkdir ncfiles')
+
+  # Read in the yaml file
+  diag_config_yml = yaml.load(open(args.diag_config_yml_path,'r'), Loader=yaml.Loader)
+
+  # Create the case instance
+  dcase = DiagsCase(diag_config_yml['Case'])
+  args.case_name = dcase.casename
+  args.savefigs = True; args.outdir = 'PNG'
+  RUNDIR = dcase.get_value('RUNDIR')
+  print('Run directory is:', RUNDIR)
+  print('Casename is:', dcase.casename)
+  print('Variables to be processed:', args.variables)
+  print('Number of workers to be used:', nw)
+
+  # read grid info
+  grd = MOM6grid(RUNDIR+'/'+dcase.casename+'.mom6.static.nc')
   depth = grd.depth_ocean
   # remote Nan's, otherwise genBasinMasks won't work
   depth[numpy.isnan(depth)] = 0.0
   basin_code = m6toolbox.genBasinMasks(grd.geolon, grd.geolat, depth)
-  # load data
-  ds = xr.open_mfdataset(args.infile+args.monthly,decode_times=False)
-  # convert time in years
-  ds['time'] = ds.time/365.
-  ti = args.year_start
-  tf = args.year_end
-  # check if data includes years between ti and tf
-  m6toolbox.check_time_interval(ti,tf,ds)
+  parallel, cluster, client = m6toolbox.request_workers(nw)
+  print('Reading surface dataset...')
+  startTime = datetime.now()
+  variables = args.variables
+  def preprocess(ds):
+    ''' Compute montly averages and return the dataset with variables'''
+    return ds[variables].resample(time="1Y", closed='left', \
+           keep_attrs=True).mean(dim='time', keep_attrs=True)
 
+  if parallel:
+    ds = xr.open_mfdataset(RUNDIR+'/'+dcase.casename+'.mom6.hm_*.nc', \
+         parallel=True, data_vars='minimal', chunks={'time': 12},\
+         coords='minimal', compat='override', preprocess=preprocess)
+  else:
+    ds = xr.open_mfdataset(RUNDIR+'/'+dcase.casename+'.mom6.hm_*.nc', \
+         data_vars='minimal', coords='minimal', compat='override', \
+         preprocess=preprocess)
+  print('Time elasped: ', datetime.now() - startTime)
+
+  print('\n Selecting data between {} and {}...'.format(args.start_date, args.end_date))
+  startTime = datetime.now()
+  ds_sel = ds.sel(time=slice(args.start_date, args.end_date))
+  print('Time elasped: ', datetime.now() - startTime)
+
+  print('\n Computing time mean...')
+  startTime = datetime.now()
+  ds_sel = ds_sel.mean('time').load()
+  print('Time elasped: ', datetime.now() - startTime)
+
+  if parallel:
+    print('\n Releasing workers...')
+    client.close(); cluster.close()
+
+  print('Saving netCDF files...')
+  ds_sel.to_netcdf('ncfiles/'+dcase.casename+'_heat_transport.nc')
   # create a ndarray subclass
   class C(numpy.ndarray): pass
 
   varName = 'T_ady_2d'
   if varName in ds.variables:
-    tmp = numpy.ma.masked_invalid(ds[varName].sel(time=slice(ti,tf)).mean('time').data)
+    tmp = numpy.ma.masked_invalid(ds_sel[varName].values)
     tmp = tmp[:].filled(0.)
     advective = tmp.view(C)
     advective.units = ds[varName].units
@@ -62,7 +111,7 @@ def main(stream=False):
 
   varName = 'T_diffy_2d'
   if varName in ds.variables:
-    tmp = numpy.ma.masked_invalid(ds[varName].sel(time=slice(ti,tf)).mean('time').data)
+    tmp = numpy.ma.masked_invalid(ds_sel[varName].values)
     tmp = tmp[:].filled(0.)
     diffusive = tmp.view(C)
     diffusive.units = ds[varName].units
@@ -72,7 +121,7 @@ def main(stream=False):
 
   varName = 'T_lbm_diffy'
   if varName in ds.variables:
-    tmp = numpy.ma.masked_invalid(ds[varName].sel(time=slice(ti,tf)).sum('z_l').mean('time').data)
+    tmp = numpy.ma.masked_invalid(ds_sel[varName].sum('z_l').values)
     tmp = tmp[:].filled(0.)
     diffusive = diffusive + tmp.view(C)
   else:
@@ -113,14 +162,14 @@ def plt_heat_transport_model_vs_obs(advective, diffusive, basin_code, grd, args)
   GandW['Atlantic'] = Atlantic
   GandW['IndoPac'] = IndoPac
 
-  if args.case_name != '':  suptitle = args.case_name + ' ' + args.label
-  else: suptitle = args.label
+  if args.case_name != '':  suptitle = args.case_name
+  else: suptitle = ''
 
   # Global Heat Transport
   plt.figure(figsize=(12,10))
   HTplot = heatTrans(advective,diffusive)
   yy = grd.geolat_c[:,:].max(axis=-1)
-  ave_title = ', averaged from {} to {}'.format(args.year_start, args.year_end)
+  ave_title = ', averaged from {} to {}'.format(args.start_date, args.end_date)
   plotHeatTrans(yy,HTplot,title='Global Y-Direction Heat Transport [PW]'+ave_title)
   plt.plot(yobs,NCEP['Global'],'k--',linewidth=0.5,label='NCEP')
   plt.plot(yobs,ECMWF['Global'],'k.',linewidth=0.5,label='ECMWF')
@@ -130,11 +179,11 @@ def plt_heat_transport_model_vs_obs(advective, diffusive, basin_code, grd, args)
   plt.legend(loc=0,fontsize=10)
   annotateObs()
   if diffusive is None: annotatePlot('Warning: Diffusive component of transport is missing.')
-  plt.show()
   if args.savefigs:
     objOut = args.outdir+'/'+args.case_name+'_HeatTransport_global.png'
-    plt.savefig(objOut)
-
+    plt.savefig(objOut); plt.close()
+  else:
+    plt.show()
   # Atlantic Heat Transport
   m = 0*basin_code; m[(basin_code==2) | (basin_code==4) | (basin_code==6) | (basin_code==7) | (basin_code==8)] = 1
   plt.figure(figsize=(12,10))
@@ -150,11 +199,11 @@ def plt_heat_transport_model_vs_obs(advective, diffusive, basin_code, grd, args)
   plt.legend(loc=0,fontsize=10)
   annotateObs()
   if diffusive is None: annotatePlot('Warning: Diffusive component of transport is missing.')
-  plt.show()
   if args.savefigs:
-    objOut = args.outdir+'/'+args.case_name+'_HeatTransport_global.png'
-    plt.savefig(objOut)
-
+    objOut = args.outdir+'/'+args.case_name+'_HeatTransport_Atlantic.png'
+    plt.savefig(objOut); plt.close()
+  else:
+    plt.show()
   # Indo-Pacific Heat Transport
   m = 0*basin_code; m[(basin_code==3) | (basin_code==5)] = 1
   plt.figure(figsize=(12,10))
@@ -170,11 +219,11 @@ def plt_heat_transport_model_vs_obs(advective, diffusive, basin_code, grd, args)
   if diffusive is None: annotatePlot('Warning: Diffusive component of transport is missing.')
   plt.suptitle(suptitle)
   plt.legend(loc=0,fontsize=10)
-  plt.show()
   if args.savefigs:
-    objOut = args.outdir+'/'+args.case_name+'_HeatTransport_global.png'
-    plt.savefig(objOut)
-
+    objOut = args.outdir+'/'+args.case_name+'_HeatTransport_IndoPacific.png'
+    plt.savefig(objOut); plt.close()
+  else:
+    plt.show()
   return
 
 def heatTrans(advective, diffusive=None, vmask=None):
@@ -228,3 +277,4 @@ def plotGandW(lat,trans,err):
 
 if __name__ == '__main__':
   main()
+
