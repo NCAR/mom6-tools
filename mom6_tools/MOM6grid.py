186cc110ec51ca051b04b0c7b89e0b51266c745c
Gustavo Marques <gmarques@ucar.edu>
2020-11-10 14:09:25 -0700

    Fixes non-monotonic longitudes
    * does not make any difference if longitudes are monotonic
    * useful to make gx1v7 plots

--- mom6_tools/MOM6grid.py
+++ mom6_tools/MOM6grid.py
@@ -21,8 +21,17 @@ def MOM6grid(grd_file, xrformat=False):
     # open grid file
     try: nc = xr.open_dataset(grd_file, decode_times=False)
     except: raise Exception('Could not find file', grd_file)
+    # fixes non-monotonic longitudes
+    mgeolon = xr.where(nc.geolon < nc.geolon[-1,0],nc.geolon+360,nc.geolon).rename({'mgeolon'})
+    mgeolon_u = xr.where(nc.geolon_u < nc.geolon_u[-1,0],nc.geolon_u+360.0,nc.geolon_u).rename({'mgeolon_u'})
+    mgeolon_c = mgeolon_u.rename({'mgeolon_c'})
+    mgeolon_v = mgeolon.rename({'mgeolon_v'})
 
     if xrformat:
+      nc['mgeolon']   = mgeolon
+      nc['mgeolon_c'] = mgeolon_c
+      nc['mgeolon_u'] = mgeolon_u
+      nc['mgeolon_v'] = mgeolon_v
       print('MOM6 grid successfully loaded... \n')
       return nc
 
@@ -38,6 +47,10 @@ def MOM6grid(grd_file, xrformat=False):
          dummy = str("MOM6_grd.%s = nc.%s[:].values"% (var,var))
          exec(dummy)
 
+      MOM6_grd.mgeolon   = mgeolon.values
+      MOM6_grd.mgeolon_c = mgeolon_c.values
+      MOM6_grd.mgeolon_u = mgeolon_u.values
+      MOM6_grd.mgeolon_v = mgeolon_v.values
       # close netcdf file
       nc.close()
       print('MOM6 grid successfully loaded... \n')
