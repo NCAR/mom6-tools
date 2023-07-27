06db44e7e2b3def562ed4766526f90eaa1c0ac8b
Gustavo Marques <gmarques@ucar.edu>
2023-07-27 12:02:23 -0600

    Add function weighted_temporal_mean
    This function computes a weighted temporal mean using the number
    of days on each month.

--- mom6_tools/m6toolbox.py
+++ mom6_tools/m6toolbox.py
@@ -17,6 +17,35 @@ def check_time_interval(ti,tf,nc):
 
  return
 
+def weighted_temporal_mean(ds, var):
+    """
+    weight by days in each month
+    """
+    # Determine the month length
+    month_length = ds.time.dt.days_in_month
+
+    # Calculate the weights
+    wgts = month_length.groupby("time.year") / month_length.groupby("time.year").sum()
+
+    # Make sure the weights in each year add up to 1
+    np.testing.assert_allclose(wgts.groupby("time.year").sum(xr.ALL_DIMS), 1.0)
+
+    # Subset our dataset for our variable
+    obs = ds[var]
+
+    # Setup our masking for nan values
+    cond = obs.isnull()
+    ones = xr.where(cond, 0.0, 1.0)
+
+    # Calculate the numerator
+    obs_sum = (obs * wgts).resample(time="AS").sum(dim="time")
+
+    # Calculate the denominator
+    ones_out = (ones * wgts).resample(time="AS").sum(dim="time")
+
+    # Return the weighted average
+    return obs_sum / ones_out
+
 def add_global_attrs(ds,attrs):
   """
   Adds global attributes to a xarray dataset or dataarray.
